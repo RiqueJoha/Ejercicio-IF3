9a7e1cc7c5efa96e745e5e4819547198276fd992
Johanna Riquelme <riquelme.johanna07@gmail.com>
2023-05-08 00:58:38 -0300

    Mejorado

--- repoif3.js
+++ repoif3.js
@@ -1,4 +1,4 @@
-
+/* 
 
 
 
@@ -12,4 +12,34 @@ if ((numeroIngresado1 == numeroIngresado2) && (numeroIngresado2 == numeroIngresa
 }else if (((numeroIngresado1 == numeroIngresado2) || (numeroIngresado1 == numeroIngresado3))||(numeroIngresado2== numeroIngresado3)) {
     document.write ("Felicidades, Ganaste el Segundo premio!");
 }else{
-    document.write ("Felicidades, Ganaste un Helado!");}
+    document.write ("Felicidades, Ganaste un Helado!");} */
+
+const numIngresado1= document.querySelector("#numeroIngresado1");
+const numIngresado2= document.querySelector("#numeroIngresado2");
+const numIngresado3= document.querySelector("#numeroIngresado3");
+const pResultado= document.querySelector ("#resultado");
+
+let numeroIngresado1 = "numero1 def";
+let numeroIngresado2 = "numero2 def";
+let numeroIngresado3 = "numero3 def";
+
+
+function verificar(){
+    /* console.log("ganaste el primer premio") */
+    numeroIngresado1=numIngresado1.value;
+    numeroIngresado2=numIngresado2.value;
+    numeroIngresado3=numIngresado3.value;
+
+    if ((numeroIngresado1 == numeroIngresado2) && (numeroIngresado2 == numeroIngresado3)) {
+
+        pResultado.innerHTML= "¡Felicidades, Ganaste el Primer premio!";
+
+    }else if (((numeroIngresado1 == numeroIngresado2) || (numeroIngresado1 == numeroIngresado3))||(numeroIngresado2== numeroIngresado3)) {
+       
+       pResultado.innerHTML= "¡Felicidades, Ganaste el Segundo premio!";
+
+    }else{
+    
+    pResultado.innerHTML= "¡Felicidades, Ganaste un Helado!";
+
+}}
